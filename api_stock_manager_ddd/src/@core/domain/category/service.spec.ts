b67ec116d2ed769f345f6497e0bd5352925f9bfb
Gustavo Torregrosa Costa <gustavo@Gustavos-MacBook-Air.local>
2023-07-10 13:32:05 -0300

    parcial

--- api_stock_manager_ddd/src/@core/domain/category/service.spec.ts
+++ api_stock_manager_ddd/src/@core/domain/category/service.spec.ts
@@ -1,8 +1,26 @@
 import { CategoryService } from "./service"
-import { CategoryRepository } from "src/infra/category/repository"
-import { ProductRepository } from "src/infra/product/repository"
+import { CategoryRepository } from "../../../infra/category/repository";
+// import { ProductRepository } from "src/infra/product/repository"
+import { ProductRepository } from "../../../infra/product/repository";
+import { Test } from '@nestjs/testing';
 
 describe('resolver',  () => {
+
+
+    // let categoryService: CategoryService
+    // let categoryRepository: CategoryRepository
+    // let productRepository: ProductRepository
+
+    // beforeAll(async() => {
+    //     const moduleTest = await Test.createTestingModule({
+    //         providers: [CategoryService, CategoryRepository, ProductRepository]
+    //     }).compile()
+
+    //     categoryService = moduleTest.get<CategoryService>(CategoryService)
+
+    // })
+   
+
 
     let categoryService: CategoryService
     let categoryRepository = new CategoryRepository()
@@ -13,4 +31,19 @@ describe('resolver',  () => {
         let listAll = await categoryService.getAll()
         
     })
+
+    // let catsController: CatsController;
+    // let catsService: CatsService;
+
+    // beforeEach(async () => {
+    //     const moduleRef = await Test.createTestingModule({
+    //         controllers: [CatsController],
+    //         providers: [CatsService],
+    //     }).compile();
+
+    //     catsService = moduleRef.get<CatsService>(CatsService);
+    //     catsController = moduleRef.get<CatsController>(CatsController);
+    // });
+
+
 })
---
+++ api_stock_manager_ddd/src/infra/category/service.spec.ts
@@ -0,0 +1,5 @@
+describe('category service', () => {
+    it('get all category', async () => {
+        
+    })
+})
